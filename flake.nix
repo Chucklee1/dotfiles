255a1cbd05c7bb2c34f5c00ed739a68dcae5d1d1
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-10-28 23:02:08 -0700

    big changes

--- flake.nix
+++ flake.nix
@@ -7,37 +7,44 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    hyprland.url = "github:hyprwm/Hyprland";
-    
-    # stylix.url = "githun";
+    };      
+    stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    
-    in 
-    {
-      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./host/desktop/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];   
-      };
-      home-manager.nixosModules.default = ./home.nix;
-      
-      # nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      # specialArgs = {inherit inputs;};
-      # modules = [
-      #   ./configuration.nix
-      #   ./host/laptop/hardware-configuration.nix
-      #    inputs.home-manager.nixosModules.default
-      # ];   
-      # }; 
-       
-    };  
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    # Function for dynamic host profiles
+    makeNixosConfig = host: {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/${host}/configuration.nix
+        inputs.stylix.nixosModules.stylix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.goat = import ./home.nix;
+        }
+      ];
+    };
+
+  in 
+  {
+    # specific hosts configs
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem (makeNixosConfig "desktop");
+      laptop = nixpkgs.lib.nixosSystem (makeNixosConfig "laptop");
+    };
+  };
 }
+# toggleable modules eg:
+# options = {
+#  module1.enable = lib.mkEnableOption = "enable option 1";
+#};
+
+#config = lib.mkIf.config.module1.enable {
+# option1 = 5;
+# option2 = true;
--- home.nix
+++ home.nix
@@ -1,23 +1,48 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.username = "goat";
   home.homeDirectory = "/home/goat";
-  
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    systemd.enable = true;
-    plugins = [ hyprscroller ];
-  }; 
-  
+  home.stateVersion = "24.05";
+   
+  gtk = {
+    iconTheme.name = "Papirus-Dark";
+    iconTheme.package = pkgs.papirus-icon-theme;
+  };
+              
+  programs = {
+    kitty = {  
+      enable = true;
+      settings = {
+		scrollback_lines = 2000;
+		wheel_scroll_min_lines = 1;
+        window_padding_width = 4;
+		confirm_os_window_close = 0;
+      };
+      extraConfig = ''
+		     tab_bar_style fade
+		     tab_fade 1
+		     active_tab_font_style   bold
+		     inactive_tab_font_style bold
+      '';
+    };
+    bash = {
+      enable = true;
+      shellAliases = {
+        sv = "sudo nvim";
+        v = "nvim";    
+        cg = "sudo nix-collect-garbage";
+        niri = "niri --session -c $HOME/goat/configs/config.kdl";
+        update-desktop = "sudo nixos-rebuild switch --impure --flake ~/goat#desktop";
+        udate-laptop = "sudo nixos-rebuild switch --impure --flake ~/goat#laptop";
+      };
+		};
+ 	};
+
   home.sessionVariables = {
     EDITOR = "neovim";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  
-  # DO NOT CHANGE
-  home.stateVersion = "24.05"; 
 }
