f482794b4908ae68635c7d73c2dc2262297a08a1
Chucklee1 <cooperkang4@gmail.com>
2024-11-01 11:12:05 -0700

    added qemu with uefi script

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -4,11 +4,19 @@
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
     programs.dconf.enable = true;
-    
+
     dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
         autoconnect = ["qemu:///system"];
         uris = ["qemu:///system"];
     };
-    };
+
+    enviorment.systemPackages = [ 
+      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
+    ];
+  };
 }
