76c0337c1f943bfb220d7f32ee3044d4e7c56654
Chucklee1 <cooperkang4@gmail.com>
2024-11-01 11:10:21 -0700

    added dconf enable to virt nix

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -3,7 +3,8 @@
 {
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
-
+    programs.dconf.enable = true;
+    
     dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
         autoconnect = ["qemu:///system"];
