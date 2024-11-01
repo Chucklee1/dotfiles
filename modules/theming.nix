1ec56bc6f32f983653a01911bf7d93184c2ff68d
Chucklee1 <cooperkang4@gmail.com>
2024-11-01 10:39:28 -0700

    added polkit options and some misc comments

--- modules/theming.nix
+++ modules/theming.nix
@@ -4,7 +4,9 @@
   # lazy theming
   stylix = {
     enable = true;
-    image = /home/goat/goat/configs/wallpapers/clouds-sunset.jpg;
+    # needed option, just ensure the image exist
+    image = /home/goat/goat/configs/wallpapers/image.jpg;
+    # actual color scheme
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ocean.yaml";
     opacity.terminal = 0.8;
     cursor.package = pkgs.bibata-cursors;
--- modules/wayland.nix
+++ modules/wayland.nix
@@ -36,7 +36,6 @@
     libnotify 
     dunst
     libsecret 
-    lxqt.lxqt-policykit
     swayidle 
     swaylock-effects 
     wlogout 
@@ -61,29 +60,33 @@
   ];
 
   # Security / Polkit
-  security.rtkit.enable = true;
-  security.polkit.enable = true;
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (
-        subject.isInGroup("users")
-          && (
-            action.id == "org.freedesktop.login1.reboot" ||
-            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-            action.id == "org.freedesktop.login1.power-off" ||
-            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+  security = {
+    rtkit.enable = true;
+    security.polkit.enable = true;
+    polkit.package = pkgs.lxqt.policykit;
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if (
+          subject.isInGroup("users")
+            && (
+              action.id == "org.freedesktop.login1.reboot" ||
+              action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+              action.id == "org.freedesktop.login1.power-off" ||
+              action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+            )
           )
-        )
-      {
-        return polkit.Result.YES;
-      }
-    })
-  '';
-  security.pam.services.swaylock = {
-    text = ''
-      auth include login
+        {
+          return polkit.Result.YES;
+        }
+      })
     '';
+    security.pam.services.swaylock = {
+      text = ''
+        auth include login
+      '';
+    };
   };
+
 
   # xdg portal
   xdg.portal = {
