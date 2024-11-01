f83fe214dfbace1e63fec016f3e0953b97b3b66d
Chucklee1 <cooperkang4@gmail.com>
2024-11-01 11:13:42 -0700

    virt typo

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -4,19 +4,10 @@
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
     programs.dconf.enable = true;
-
     dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-        autoconnect = ["qemu:///system"];
-        uris = ["qemu:///system"];
+      "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
     };
-
-    enviorment.systemPackages = [ 
-      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-        qemu-system-x86_64 \
-          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-          "$@"
-      '')
-    ];
-  };
+};
 }
